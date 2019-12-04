2e4af2d2fb52e6bfa12bfeba19dc3231bf69cea5
milkyeway <52460221+milkyeway@users.noreply.github.com>
2019-12-04 17:55:07 +0800

    Add files via upload

---
+++ Guests/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Home_guests from './components/Home/Home_guests'
+const App = () => {
+  return (
+    <>
+      <Home_guests />
+    </>
+  )
+}
+export default App
