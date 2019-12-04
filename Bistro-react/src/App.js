928113ef075a58dfd1d11719769b04072c629fd6
milkyeway <52460221+milkyeway@users.noreply.github.com>
2019-12-04 18:01:50 +0800

    Add files via upload

---
+++ Bistro-react/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Navigation_Navber_Home from './components/Navigation_Navber/Navigation_Navber_Home'
+const App = () => {
+  return (
+    <>
+      <Navigation_Navber_Home />
+    </>
+  )
+}
+export default App
